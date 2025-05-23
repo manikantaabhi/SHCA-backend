3f4c7336297bc839713330c76bf9521e7c3fb02d
manikantaabhi <manikanta.thayappa@gmail.com>
2025-05-23 12:53:51 -0500

    fixing error

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const Inquiry = require('../models/Inquiry');
 const Review = require('../models/Review');
 
